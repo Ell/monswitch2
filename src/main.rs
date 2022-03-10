d7833c2386e92faf108059a6033f8952b3c8af50
Ell <alan@cattes.us>
2022-03-10 12:48:57 -0600

    initial work

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+#[macro_use]
+extern crate lazy_static;
+
+use iced::{Align, Application, Column, Container, Length, Settings, Text};
+use monitor::Monitor;
+
+mod monitor;
+
+fn main() -> iced::Result {
+    Monswitch::run(Settings::default())
+}
+
+#[derive(Debug, Clone)]
+enum Error {
+    MonitorError,
+}
+
+#[derive(Debug)]
+enum Monswitch<'a> {
+    Loading,
+    Loaded { monitors: Vec<Monitor> },
+    HasError,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    MonitorsLoaded(Result<Vec<Monitor>, Error>),
+    ReloadMonitors,
+}
+
+impl Application for Monswitch {
+    type Executor = iced::executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        (
+            Monswitch::Loading,
+            iced::Command::perform(Monitor::get_monitors(), Message::MonitorsLoaded),
+        )
+    }
+
+    fn title(&self) -> String {
+        "monswitch".to_string()
+    }
+
+    fn update(
+        &mut self,
+        message: Self::Message,
+        _clipboard: &mut iced::Clipboard,
+    ) -> iced::Command<Self::Message> {
+        match message {
+            Message::MonitorsLoaded(Ok(monitors)) => {
+                *self = Monswitch::Loaded { monitors };
+
+                iced::Command::none()
+            }
+            Message::MonitorsLoaded(Err(_error)) => {
+                *self = Monswitch::HasError;
+
+                iced::Command::none()
+            }
+            Message::ReloadMonitors => match self {
+                Monswitch::Loading => iced::Command::none(),
+                _ => {
+                    *self = Monswitch::Loading;
+
+                    iced::Command::perform(Monitor::get_monitors(), Message::MonitorsLoaded)
+                }
+            },
+        }
+    }
+
+    fn view(&mut self) -> iced::Element<'_, Self::Message> {
+        let content = match self {
+            Monswitch::Loading => Column::new()
+                .width(Length::Shrink)
+                .push(Text::new("Gathering Monitor Information").size(40)),
+            Monswitch::Loaded { monitors } => {
+                let mut column = Column::new().spacing(20).align_items(Align::Start);
+
+                for monitor in monitors {
+                    column = column.push(monitor.view());
+                }
+
+                column
+            }
+            Monswitch::HasError => Column::new()
+                .width(Length::Shrink)
+                .push(Text::new("Error Occured").size(40)),
+        };
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
+    }
+}
---
+++ src/monitor.rs
@@ -0,0 +1,59 @@
+use std::{borrow::Borrow, sync::Arc};
+
+use anyhow::Result;
+use futures::lock::Mutex;
+use iced::{Align, Column, Element, Image, Length, Row, Text};
+
+use crate::{Error, Message};
+
+lazy_static! {
+    static ref DEFAULT_IMAGE_HANDLE: iced::image::Handle =
+        iced::image::Handle::from_memory(include_bytes!("default.png").to_vec());
+}
+
+#[derive(Debug, Clone)]
+pub struct Monitor<'a> {
+    id: String,
+    model_name: Option<String>,
+}
+
+impl<'a> Monitor<'a> {
+    pub(crate) async fn get_monitors() -> Result<Vec<Monitor<'a>>, Error> {
+        let monitors: Vec<_> = ddc_hi::Display::enumerate()
+            .iter()
+            .map(|display| {
+                let model_name = display.info.model_name.clone();
+                let id = display.info.id.clone();
+
+                Monitor { id, model_name }
+            })
+            .collect();
+
+        Ok(monitors)
+    }
+
+    pub(crate) fn view(&mut self) -> Element<Message> {
+        let model_name = match &self.model_name {
+            Some(model_name) => model_name.clone(),
+            None => self.id.clone(),
+        };
+
+        Row::new()
+            .spacing(20)
+            .align_items(Align::Center)
+            .push(Monitor::fetch_image(&model_name))
+            .push(
+                Column::new()
+                    .align_items(Align::Start)
+                    .width(Length::Fill)
+                    .spacing(5)
+                    .push(Text::new(model_name).size(30).width(Length::Fill))
+                    .push(Text::new(self.id.clone()).size(20).width(Length::Fill)),
+            )
+            .into()
+    }
+
+    fn fetch_image(_model_name: &str) -> Image {
+        Image::new(DEFAULT_IMAGE_HANDLE.clone())
+    }
+}
